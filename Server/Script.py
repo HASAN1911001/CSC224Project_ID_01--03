f9120d34778c96da3a992927b74ab3ce41b210a8
Mr. Zi <md.ziaulhasan00@gmail.com>
2023-01-27 18:50:47 +0600

    Primary Algorithm

---
+++ Server/Script.py
@@ -0,0 +1,17 @@
+# import required modules
+
+# check whether or not there are any file of required types
+
+    # if the files are not in the intermideate folder, save them to the intermdeiate folder
+    # if the file is .py run it
+
+    # if the files are in the intermediate folder, check if any changes is made
+
+        # if no changes are made, ignor
+
+        # if changes are made, notify the user
+
+            # if user wants to save the changes, save the file to intermediate folder
+
+            # if he does not want to change it, replace the file from the intermediate folder
+
