cb95f5927df3547209e1092bc258058ddb6849df
Mr. Zi <md.ziaulhasan00@gmail.com>
2023-01-27 19:39:58 +0600

    files are taken

--- Server/Script.py
+++ Server/Script.py
@@ -1,6 +1,20 @@
 # import required modules
+from glob import glob
+import shutil
 
 # check whether or not there are any file of required types
+source_path = '../Server/*'
+intermediate_path = '../Intermediate'
+
+source_object = glob(source_path)
+
+if len(source_object) > 1:
+    for i in range(1, len(source_object)):
+        object_path = source_object[i]
+        object_name = object_path.split('\\')[-1].split('.')
+        prefix = object_name[0]
+        postfix2 = object_name[1]
+
 
     # if the files are not in the intermideate folder, save them to the intermdeiate folder
     # if the file is .py run it
