0b8c66ba1d05236d72f6228dc9331f92a198ced3
Mr. Zi <md.ziaulhasan00@gmail.com>
2023-01-31 22:11:18 +0600

    Updated

--- Server/Script.py
+++ Server/Script.py
@@ -2,7 +2,6 @@
 from glob import glob
 from subprocess import call
 
-# check whether or not there are any file of required types
 source_path = '../Server/*'
 intermediate_path = '../Intermediate/*'
 
@@ -19,24 +18,30 @@ for i in range(len(intermediate_object)):
     intermediate_files.append(prefix)
 
 
+# Check if there any file other then Script file
 if len(source_object) > 1:
+    # For every file
     for i in range(1, len(source_object)):
         object_path = source_object[i]
         object_name = object_path.split('\\')[-1].split('.')
         prefix = object_name[0]
         postfix2 = object_name[1]
         
-        
+        path = '.'.join(object_name)
         # if the file is .py run it
         if postfix2 == 'py' and prefix != 'Script':
+            print()
+            print("==================================================")
+            print(f"{path} is running from Script.py")
+            print("Output:")
             call(["python", "{}".format('.'.join(object_name))])
-        
+            print("==================================================")
+            print()
         
         
         if postfix2 == 'py' or postfix2 == 'c' or postfix2 == 'txt':
 
             # if the files are in the intermediate folder, check if any changes is made
-            path = '.'.join(object_name)
             if prefix in intermediate_files:
                 with open(path, 'r') as f:
                     file = f.readlines()
@@ -46,7 +51,13 @@ if len(source_object) > 1:
 
                 # if changes are made, notify the user
                 if file != iFile:
-                    opinion = input(f"There are some changes in the file {path}. Do you want to keep the changed file: ")
+                    print()
+                    print("===============================================")
+                    print("There are some changes in the file {path}!")
+                    print("Do yo want to keep the changed file?")
+                    opinion = input("Y or N: ")
+                    print("===============================================")
+                    print()
                     
                     # if user wants to save the changes, save the file to intermediate folder
                     if opinion == 'Y':
@@ -68,3 +79,16 @@ if len(source_object) > 1:
                 with open(f"../Intermediate/{path}", 'w') as f:
                     for i in range(len(file)):
                         f.write(''.join(file[i]))
+
+
+
+#Algorithm:
+
+# Check if there any file other then Script file
+# For every file
+# if the file is .py run it
+# if the files are in the intermediate folder, check if any changes is made
+# if changes are made, notify the user
+# if user wants to save the changes, save the file to intermediate folder
+# if he does not want to change it, replace the file from the intermediate folder
+# if the file is not in the intermideate folder, save it to the intermdeiate folder
